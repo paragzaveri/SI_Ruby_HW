2572932b955c4f6bbe80a104e1f60a335f93da5f
Parag Zaveri <parag.g.zaveri@gmail.com>
2015-06-11 11:35:57 -0500

    Added Entering League Games Manually

--- Week_6/leaguepointcalculator.rb
+++ Week_6/leaguepointcalculator.rb
@@ -18,8 +18,43 @@ module Leaderboard
 	end
 end
 
+module UpdateScores
+	def calculate_league_points(first_team,second_team,first_team_score,second_team_score,league)
+		if first_team_score > second_team_score
+			puts "\nThe #{first_team} Win!"
+			league["#{first_team}"] += 3
+			league["#{second_team}"] += 0
+		elsif second_team_score > first_team_score
+			puts "\nThe #{second_team} Win!"
+			league["#{second_team}"] += 3
+			league["#{first_team}"] += 0
+		else
+			puts "\nIt's a tie!"
+			league["#{first_team}"] += 1
+			league["#{second_team}"] += 1
+		end
+	end
+
+	def calculate_interleague_points(first_team,second_team,first_team_score,second_team_score,league_1,league_2)
+		if first_team_score > second_team_score
+			puts "\nThe #{first_team} Win!"
+			league_1["#{first_team}"] += 3
+			league_2["#{second_team}"] += 0
+		elsif second_team_score > first_team_score
+			puts "\nThe #{second_team} Win!"
+			league_2["#{second_team}"] += 3
+			league_1["#{first_team}"] += 0
+		else
+			puts "\nIt's a tie!"
+			league_1["#{first_team}"] += 1
+			league_2["#{second_team}"] += 1
+		end
+	end
+end
+
 module Games
 	include Leaderboard
+	include UpdateScores
 	def generate_league_game(league,name)
 		keys = league.keys
  		first_team = keys[rand(15)]
@@ -36,19 +71,8 @@ module Games
 		first_team_score = gets.chomp.to_i
 		print "#{second_team}: "
 		second_team_score = gets.chomp.to_i
-		if first_team_score > second_team_score
-			puts "\nThe #{first_team} Win!"
-			league["#{first_team}"] += 3
-			league["#{second_team}"] += 0
-		elsif second_team_score > first_team_score
-			puts "\nThe #{second_team} Win!"
-			league["#{second_team}"] += 3
-			league["#{first_team}"] += 0
-		else
-			puts "\nIt's a tie!"
-			league["#{first_team}"] += 1
-			league["#{second_team}"] += 1
-		end
+		
+		calculate_league_points(first_team,second_team,first_team_score,second_team_score,league)
 
 		print_leaderboard_league(league,name)
 
@@ -67,21 +91,77 @@ module Games
 		first_team_score = gets.chomp.to_i
 		print "#{second_team}: "
 		second_team_score = gets.chomp.to_i
-		if first_team_score > second_team_score
-			puts "\nThe #{first_team} Win!"
-			league_1["#{first_team}"] += 3
-			league_2["#{second_team}"] += 0
-		elsif second_team_score > first_team_score
-			puts "\nThe #{second_team} Win!"
-			league_2["#{second_team}"] += 3
-			league_1["#{first_team}"] += 0
-		else
-			puts "\nIt's a tie!"
-			league_1["#{first_team}"] += 1
-			league_2["#{second_team}"] += 1
-		end		
-		print_leaderboard_interleague(league_1,league_2,name_1,name_2)
+	
+		calculate_interleague_points(first_team,second_team,first_team_score,second_team_score,league_1,league_2)
 
+		print_leaderboard_interleague(league_1,league_2,name_1,name_2)
+	end
+
+	def enter_league_game(league,name,array)
+		puts "\nTeams to choose from:"
+		puts ""
+		puts name
+		puts ""
+		puts array
+		print "\n\nEnter first team name (case-sensitive): "
+		first_team = gets.chomp
+		if array.include?(first_team)
+			print "How many runs: "
+			first_team_score = gets.chomp.to_i
+			puts ""
+		else
+			puts "\n\nSorry not a valid team name, try again."
+			enter_league_game(league,name,array)
+		end
+		print "\nEnter second team name (case-sensitive): "
+		second_team = gets.chomp
+		if array.include?(second_team)
+			print "How many runs: "
+			second_team_score = gets.chomp.to_i
+			puts ""
+		else
+			puts "\n\nSorry not a valid team name, try again. (re-enter both teams)"
+			enter_league_game(league,name,array)
+		end
+
+		calculate_league_points(first_team,second_team,first_team_score,second_team_score,league)
+
+		print_leaderboard_league(league,name)
+	end
+
+	def enter_interleague_game(league_1,league_2,name_1,name_2,array_1,array_2)
+		puts "\nTeams to choose from:"
+		puts ""
+		puts name_1
+		puts ""
+		puts array_1
+		print "\n\nEnter first team name (case-sensitive): "
+		first_team = gets.chomp
+		if array_1.include?(first_team)
+			print "How many runs: "
+			first_team_score = gets.chomp.to_i
+			puts ""
+		else
+			puts "\n\nSorry not a valid team name, try again."
+			enter_interleague_game(league_1,league_2,name_1,name_2,array_1,array_2)
+		end
+		puts "\nFirst team entered. Enter second team."
+		puts "\nTeams to choose from: "
+		puts name_2
+		puts ""
+		puts array_2
+		print "\nnter second team name (case_sensitive): "
+		second_team = gets.chomp
+		if array_2.include?(second_team)
+			print "How many runs: "
+			second_team_score = gets.chomp.to_i
+			puts ""
+		else
+			puts "\n\nSorry not a valid team name, try again."
+			enter_interleague_game(league_1,league_2,name_1,name_2,array_1,array_2)
+		end
+		calculate_interleague_points(first_team,second_team,first_team_score,second_team_score,league_1,league_2)
+		print_leaderboard_interleague(league_1,league_2,name_1,name_2)
 	end
 end
 
@@ -90,8 +170,8 @@ class LPC
 	include Leaderboard
 	def initialize(name)
 		@name = name
-		puts "Welcome to the League Point Calculator #{@name}!"
-		puts "\nHere is a list of the teams in the league."
+		puts "\nWelcome to the League Point Calculator #{@name}!"
+		puts "\nHere is a list of all the teams in both leagues."
 		@american_league_hash = {
 			"Baltimore Orioles" => @orioles_points=0,
 			"Boston Red Sox" => @redsox_points=0,
@@ -140,7 +220,7 @@ class LPC
 	end
 
 	def input_scores()
-		print "\n\n\nWould you like to input scores? (0 for league play, 1 for interleague play, 2 to exit): " 
+		print "\n\n\nWould you like to input scores? ([0]:League Play, [1]:Interleague Play, [2]:Exit): " 
 		user_input = gets.chomp.to_i
 		if (user_input == 0)
 			league_play
@@ -162,18 +242,36 @@ class LPC
 	end
 
 	def american_league()
-		generate_league_game(@american_league_hash,@american_league_name)
+		print "\nGenerate League Game (0) or Enter League Game (1): "
+		user_input = gets.chomp.to_i
+		if user_input == 0
+			generate_league_game(@american_league_hash,@american_league_name)
+		else
+			enter_league_game(@american_league_hash,@american_league_name,@american_league_array)
+		end
 		input_scores
 	end
 
 	def national_league()
-		generate_league_game(@national_league_hash,@national_league_name)
+		print "\nGenerate League Game (0) or Enter League Game (1): "
+		user_input = gets.chomp.to_i
+		if user_input == 0
+			generate_league_game(@national_league_hash,@national_league_name)
+		else
+			enter_league_game(@national_league_hash,@national_league_name,@national_league_array)
+		end
 		input_scores
 
 	end
 
 	def interleague_play()
-		generate_interleague_game(@american_league_hash,@national_league_hash,@american_league_name,@national_league_name)
+		print "\nGenerate Interleague Game (0) or Enter Interleague Game (1): "
+		user_input = gets.chomp.to_i
+		if user_input == 0
+			generate_interleague_game(@american_league_hash,@national_league_hash,@american_league_name,@national_league_name)
+		else
+			enter_interleague_game(@american_league_hash,@national_league_hash,@american_league_name,@national_league_name,@american_league_array,@national_league_array)
+		end
 		input_scores
 	end
 
@@ -181,7 +279,7 @@ class LPC
 		puts "\n\n\nFinal League Scores:"
 		print_leaderboard_interleague(@american_league_hash,@national_league_hash,@american_league_name,@national_league_name)
 
-		puts "Hope you had a good time!  Goodbye!"
+		puts "\n\nHope you had a good time!  Goodbye!"
 	end
 end
 
